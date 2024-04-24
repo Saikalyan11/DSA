8e4563e55e51a18ef1e4d1f571af22d6180720ae
Saikalyan11 <kalyanw9700128@gmail.com>
2024-04-24 19:08:53 +0530

    GCD using Eculidean Algorithm for better complexity

--- Basic Math/GCDusingEuclidean.java
+++ Basic Math/GCDusingEuclidean.java
@@ -2,6 +2,10 @@ public class GCDusingEuclidean {
     public static void main(String[] args) {
         int n1 = 80;
         int n2 = 12;
+        // Euclidean Algorithm from line 5 to 17
+        // This is an efficient method for computing the Greatest commom divisor
+        // [GCD] of 2 integers, the largest number that divides them both without a
+        // remainder.
         while (n1 > 0 && n2 > 0) {
             if (n1 > n2) {
                 n1 = n1 % n2;
