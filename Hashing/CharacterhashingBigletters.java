afde7bffacaff363804e1996ba3e484d5e376aaf
Saikalyan11 <kalyanw9700128@gmail.com>
2024-05-16 00:54:49 +0530

    Capital and small  letters character hashing

--- Hashing/CharacterhashingBigletters.java
+++ Hashing/CharacterhashingBigletters.java
@@ -2,6 +2,17 @@ package Hashing;
 
 public class CharacterhashingBigletters {
     public static void main(String[] args) {
+        // Capital and small letters Character Hashing
+        String s = "ABCDaaaaAFbbEDA";
 
+        // pre-store or pre-compute
+        int hash[] = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            hash[s.charAt(i)] += 1;
+        }
+
+        // query
+        char c = 'A';
+        System.out.println(hash['b']);
     }
 }
