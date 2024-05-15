04691ca431442369281a9a7182e870e5b1c3ac97
Saikalyan11 <kalyanw9700128@gmail.com>
2024-05-16 00:36:19 +0530

    (Hashing DSA) counting characters repeated in a string

---
+++ Hashing/Characterhashing.java
@@ -0,0 +1,24 @@
+package Hashing;
+
+import java.util.*;
+
+public class Characterhashing {
+    public static void main(String[] args) {
+
+        // input
+        String s = "abcdaefc";
+        int len = s.length();
+        char c = 'a';
+
+        // pre-store or pre-compute
+        int hash[] = new int[26];
+        for (int i = 0; i < len; i++) {
+            hash[s.charAt(i) - c] += 1;
+        }
+
+        char q = 'f';
+        int res = q - 'a';
+        System.out.println(hash[res]);
+
+    }
+}
